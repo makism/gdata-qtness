5143ab2f40bd1704ecb1ce6c5ad05e055e895647
makis marimpis <makhsm@gmail.com>
2011-08-14 09:32:00 +0300

    * Initial code.

--- gdata-qtness-goauth/authorizetoken.cpp
+++ gdata-qtness-goauth/authorizetoken.cpp
@@ -5,11 +5,36 @@ namespace GDataQtness {
 GOAuth::AuthorizeToken::AuthorizeToken(QObject *parent) :
     QObject(parent)
 {
+    mGoogleParameters.insert("hd", "default");
+    mGoogleParameters.insert("hl", "en");
 }
 
 GOAuth::AuthorizeToken::~AuthorizeToken()
 {
 
+}
+
+void GOAuth::AuthorizeToken::setDomain(const QString &_domain)
+{
+    mGoogleParameters.insert("hd", _domain);
+}
+
+void GDataQtness::GOAuth::AuthorizeToken::setCountryCode(const QString &_countryCode)
+{
+    mGoogleParameters.insert("hl", _countryCode);
+}
+
+void GOAuth::AuthorizeToken::forceMobileVersion(bool _mobile)
+{
+    if (_mobile)
+        mGoogleParameters.insert("btml", "mobile");
+    else
+        mGoogleParameters.remove("btml");
+}
+
+void GOAuth::AuthorizeToken::setToken(const QString &_token)
+{
+    mGoogleParameters.insert("oauth_token", _token);
 }
 
 }
--- gdata-qtness-goauth/authorizetoken.h
+++ gdata-qtness-goauth/authorizetoken.h
@@ -2,22 +2,33 @@
 #define AUTHORIZETOKEN_H
 
 #include <QObject>
+#include <QMap>
+
+#include "gdata-qtness-goauth_global.h"
+#include "gdata-qtness-goauth_namespace.h"
 
 
 namespace GDataQtness {
 namespace GOAuth {
 
-class AuthorizeToken : public QObject
+class GDATAQTNESSGOAUTHSHARED_EXPORT AuthorizeToken : public QObject
 {
     Q_OBJECT
 public:
     explicit AuthorizeToken(QObject *parent = 0);
     ~AuthorizeToken();
 
+    void setToken(const QString &_token);
+    void setDomain(const QString &_domain);
+    void setCountryCode(const QString &_countryCode);
+    void forceMobileVersion(bool _mobile);
+
 signals:
 
 public slots:
 
+private:
+    QMap<QString, QString> mGoogleParameters;
 };
 
 }
